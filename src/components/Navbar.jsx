c833cadfee7172c8145311dd7fee20deef12bd30
Jyoti-jitu <parhijyotiswarup@gmail.com>
2026-01-07 19:42:49 +0530

    add underline in navbar whwn hover

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -53,7 +53,9 @@ const Navbar = () => {
                 <a
                   key={link.name}
                   href={link.href}
-                  className="text-gray-700 hover:text-blue-600 text-sm font-medium transition-colors"
+                  className="relative text-gray-700 hover:text-blue-600 text-sm font-medium transition-colors
+            after:content-[''] after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-[2px]
+            after:bg-blue-600 after:transition-all after:duration-300 hover:after:w-full"
                 >
                   {link.name}
                 </a>
